3288486e39dcfc154caa25276e5207557960fe0a
Ralf Northman <ralf.northman@bredband.net>
2018-06-18 15:09:03 +0200

    Inset word boxes.

--- src/Main.elm
+++ src/Main.elm
@@ -8,6 +8,7 @@ import Style.Font as Font
 import Style.Shadow as Shadow
 import Style.Border as Border
 import Style.Color
+import Style.Filter
 import Color
 
 
@@ -19,6 +20,7 @@ type Styles
     | Title
     | Text
     | Row
+    | RowElement
 
 
 stylesheet =
@@ -36,11 +38,27 @@ stylesheet =
         , style Text
             [ Font.typeface [ rowFont ]
             , Font.size 25
+            , Style.Color.text Color.lightYellow
+            , Shadow.text
+                { offset = ( 1.5, 2.5 )
+                , blur = 1
+                , color = Color.black
+                }
             ]
         , style Row
-            [ Border.all 5
+            [ Border.all 2
             , Border.rounded 20
-            , Style.Color.border Color.lightGrey
+            , Style.Color.border Color.black
+            , Style.Color.background Color.darkPurple
+            , Style.Filter.blur 0.18
+            ]
+        , style RowElement
+            [ Shadow.inset
+                { offset = ( 3, 5 )
+                , size = 2
+                , blur = 5
+                , color = Color.black
+                }
             ]
         ]
 
@@ -77,14 +95,20 @@ wholeRow =
     el NoStyle [ center ] wordsRow
 
 
+rowElement topPad word =
+    el RowElement [ paddingXY 10 0 ] <|
+        el Text [ paddingTop topPad ] <|
+            text word
+
+
 wordsRow =
     row Row
         [ paddingXY 40 15, spacing 40 ]
-        [ el Text [] <| text "Separated"
-        , el Text [ paddingTop 10 ] <| text "Words"
-        , el Text [] <| text "on"
-        , el Text [ paddingTop 10 ] <| text "a"
-        , el Text [] <| text "Row"
+        [ rowElement 0 "Separated"
+        , rowElement 10 "Words"
+        , rowElement 0 "on"
+        , rowElement 10 "a"
+        , rowElement 0 "Row"
         ]
 
 
